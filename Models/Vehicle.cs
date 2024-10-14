e6c3439cf09c5b0258253ba4292b972d253e7001
Anesu Kabuku <125449219+kabukuanesu@users.noreply.github.com>
2024-10-14 15:08:05 +0200

    Add project files.

---
+++ Models/Vehicle.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FleetManagementAPI.Models
+{
+    public class Vehicle
+    {
+        [Key]
+        public int VehicleId { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string VehicleRegistrationNumber { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string VehicleName { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string VehicleModel { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string VehicleChassisNumber { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string VehicleEngineNumber { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string VehicleManufacturedBy { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string VehicleType { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string VehicleColor { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string VehicleMilagePerLitre { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string VehicleIsActive { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string VehicleRegistrationExpiryDate { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string VehicleCreatedBy { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string VehicleCreatedDate { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string VehicleModifiedDate { get; set; } = "";
+    }
+}
